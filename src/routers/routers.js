1b70210e028ec271d41a8ddbc6afd9ead6728b14
Bikal <bikaladhikari5@gmail.com>
2024-07-24 17:50:37 +1000

    userRouter created

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,8 +1,8 @@
-// import userRouter from "./userRouter.js";
+import userRouter from "./userRouter.js"
 
 export default [
   {
     path: "api/v1/users",
-    // middlewares: [userRouter],
+    middlewares: [userRouter],
   },
 ];
