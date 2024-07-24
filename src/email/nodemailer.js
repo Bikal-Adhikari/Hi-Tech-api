38cfdb3f46e7e3fcbe960d51df7e8d7ba0f20a84
Bikal <bikaladhikari5@gmail.com>
2024-07-24 18:07:58 +1000

    nodemailer created to send user email verification

---
+++ src/email/nodemailer.js
@@ -0,0 +1,60 @@
+import nodemailer from "nodemailer";
+
+const emailProcessor = async (mailBodyObj) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_SERVER,
+      port: 587,
+      secure: false, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+
+    const info = await transporter.sendMail(mailBodyObj);
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// async..await is not allowed in global scope, must use a wrapper
+export const emailVerificationMail = ({ email, fName, url }) => {
+  const obj = {
+    from: `"Hi-Tech Store" <${process.env.SMTP_EMAIL}>`, // sender address
+    to: email, // list of receivers
+    subject: "Action Required", // Subject line
+    text: `Hellow there, please follow the link to verify you account ${url}`, // plain text body
+    html: `
+    Hello ${fName},
+<br />
+<br />
+
+<p>
+    Click the button bellow to verify your email
+   </p> 
+
+   <br />
+   <a href="${url}" style="padding: 2rem; background: green"> Verify Now
+   </a>
+
+
+<p>
+If the above button doesn't work , Please copy the following url and paste in your browser
+${url}
+</p>
+<br />
+<br />
+<p>
+Regards, <br />
+Tech Store
+</p>
+
+
+    `, // html body
+  };
+
+  emailProcessor(obj);
+};
